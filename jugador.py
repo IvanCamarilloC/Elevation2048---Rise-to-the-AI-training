4070de489b5bf77fdb5dfff14f5482411fb05eb8
Ivan Camarillo <iacamarilloc@gmail.com>
2023-05-23 10:57:28 -0600

    func: getHotEncodedMatriz

--- jugador.py
+++ jugador.py
@@ -17,14 +17,14 @@ def hotEncode(pos):
     elif pos == 3:
         return [0, 0, 0, 1]
     return [0, 0, 0, 0]
-def randomPlayer():
+
+def randomPlayer(tiempo = 0):
     partida = PartidaFactory.create_partida()
     while not partida.isOver():
         partida.play(hotEncode(random.randint(0, 3)))
     partida.showUI()
+    time.sleep(tiempo) 
 
-print("Hola")
-randomPlayer()
-print("Adios")
+randomPlayer(0)
 
     
